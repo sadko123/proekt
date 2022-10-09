cdbec639f9dbcdc18d5cfecf4bd2e32e59081a64
sadko123 <115405702+sadko123@users.noreply.github.com>
2022-10-09 19:21:02 +0600

    Add files via upload

--- maze.py
+++ maze.py
@@ -7,8 +7,25 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = sprite_x
         self.rect.y = sprite_y
-
         self.speed = sprite_speed
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def update(self):
+
+        key_pressed = key.get_pressed()
+
+        if key_pressed[K_UP] and self.rect.y < (window_hight - sprite_hight):
+            self.rect.y += self.speed
+        if key_pressed[K_DOWN] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if key_pressed[K_RIGHT] and self.rect.x < (window_widht - sprite_widht):
+            self.rect.x += self.speed
+        if key_pressed[K_LEFT] and self.rect.x > 0:
+            self.rect.x -= self.speed
+
 
 window_widht = 700
 window_hight = 500
@@ -29,12 +46,13 @@ window = display.set_mode((700, 500))
 
 background = image.load('background.jpg')
 background = transform.scale(background, (700, 500))
-sprite1 = transform.scale(image.load('hero.png'), (80,80))
-vrag = transform.scale(image.load('cyborg.png'), (80,80))
-socrovishe =  transform.scale(image.load('treasure.png'), (80,80))
+sprite1_image = 'hero.png'
+vrag_image = 'cyborg.png'
+socrovishe_image =  'treasure.png'
 
-
-
+sprite1 = Player(sprite1_image, 50, 50, 10)
+vrag = GameSprite(vrag_image, 600, 200, 5)
+socrovishe = GameSprite(socrovishe_image , 600, 400, 0)
 
 run = True
 while run:
@@ -42,10 +60,13 @@ while run:
         if i.type == QUIT:
             run = False
     
+    sprite1.update()
+
+
     window.blit(background, (0,0))
-    window.blit(sprite1, (100, 100))
-    window.blit(vrag, (500,200))
-    window.blit(socrovishe, (500, 400))
+    sprite1.reset()
+    vrag.reset()
+    socrovishe.reset()
     display.update()
     clock.tick(FPS)
    
